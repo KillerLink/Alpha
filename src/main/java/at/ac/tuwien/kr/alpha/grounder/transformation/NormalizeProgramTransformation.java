0473a90e1406a89fa53e5cf7bdcfd847ad6a28b8
Theodor M <e1426389@student.tuwien.ac.at>
2021-05-20 09:09:55 +0200

    Call meta programming implementation draft for testing.

--- src/main/java/at/ac/tuwien/kr/alpha/grounder/transformation/NormalizeProgramTransformation.java
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/transformation/NormalizeProgramTransformation.java
@@ -20,6 +20,8 @@ public class NormalizeProgramTransformation extends ProgramTransformation<InputP
 	@Override
 	public NormalProgram apply(InputProgram inputProgram) {
 		InputProgram tmpPrg;
+		// Generate Meta Program
+		InputProgram metaPrg = new MetaProgramSyntaxCheckTransformation().apply(inputProgram);
 		// Transform choice rules.
 		tmpPrg = new ChoiceHeadToNormal().apply(inputProgram);
 		// Transform cardinality aggregates.
