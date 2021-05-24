d61c490a785079b341793e8cf765bd6487cfc74f
Michael Langowski <e1426581@student.tuwien.ac.at>
2021-05-24 16:07:33 +0200

    unit tests for fact reification

--- src/test/java/at/ac/tuwien/kr/alpha/grounder/transformation/ReificationTest.java
+++ src/test/java/at/ac/tuwien/kr/alpha/grounder/transformation/ReificationTest.java
@@ -46,7 +46,7 @@ public class ReificationTest {
 				"fact(0), fact_argument(0, arg(0, const(x))), fact_predicate(0, pred(p, 1))",
 				Collections.singleton(reified));
 	}
-	
+
 	@Test
 	public void reifyIntegerFact() {
 		String asp = "q(1).";
@@ -55,23 +55,33 @@ public class ReificationTest {
 				"fact(0), fact_argument(0, arg(0, integer(1))), fact_predicate(0, pred(q, 1))",
 				Collections.singleton(reified));
 	}
-	
+
 	@Test
 	public void reifyStringFact() {
 		String asp = "q(\"bla\").";
 		AnswerSet reified = reify(asp);
 		TestUtils.assertAnswerSetsEqual(
 				"fact(0), fact_argument(0, arg(0, string(\"bla\"))), fact_predicate(0, pred(q, 1))",
-				Collections.singleton(reified));	
+				Collections.singleton(reified));
 	}
-	
+
 	@Test
 	public void reifyFuncTermFact() {
 		String asp = "r(f(x)).";
 		AnswerSet reified = reify(asp);
-		TestUtils.assertAnswerSetsEqual(
-				"fact(0), fact_argument(0, arg(0, func(0))), fact_predicate(0, pred(r, 1)), func(0), "
+		TestUtils.assertAnswerSetsEqual("fact(0), fact_argument(0, arg(0, func(0))), fact_predicate(0, pred(r, 1)), func(0), "
 				+ "func_symbol(0, symbol(f, 1)), func_argument(0, arg(0, const(x)))",
+				Collections.singleton(reified));
+	}
+
+	@Test
+	public void reifyComplexFact() {
+		String asp = "complex(1, a, f(\"bla\", g(\"blubb\")), 3).";
+		AnswerSet reified = reify(asp);
+		TestUtils.assertAnswerSetsEqual("fact(0), fact_predicate(0, pred(complex, 4)), fact_argument(0, arg(0, integer(1))), "
+				+ "fact_argument(0, arg(1, const(a))), fact_argument(0, arg(2, func(0))), fact_argument(0, arg(3, integer(3))), "
+				+ "func(0), func_symbol(0, symbol(f, 2)), func_argument(0, arg(0, string(\"bla\"))), func_argument(0, arg(1, func(1))), "
+				+ "func(1), func_symbol(1, symbol(g, 1)), func_argument(1, arg(0, string(\"blubb\")))",
 				Collections.singleton(reified));
 	}
 
